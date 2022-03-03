77635219dabf8a07a5174bcc6e34d7cd6f491198
ErikZhouXin@EZhouXin.com <ErikZhouXin@EZhouXin.com>
2022-03-03 10:19:42 +0800

    [ErikZhouXin]添加扩展调用及方法;优化方法系列;

--- demo/ut/StringUT.cs
+++ demo/ut/StringUT.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Data.Cobber;
 using System.Data.Extter;
 using System.Text;
 
@@ -16,6 +17,21 @@ namespace System.Data.DabberUT
             Console.WriteLine("aTest".ToSnakeCase());
             Console.WriteLine("a_test".SnakeToCamelCase());
             Console.WriteLine("a_test".SnakeToPascalCase());
+        }
+        [TestMethod]
+        public void TestSplit()
+        {
+            var strChar = "你,是，谁|哈 屁";
+
+            IEnumerable<Tuble2String> list = new List<Tuble2String>()
+            {
+                new Tuble2String("你是谁哈皮","11"),
+                new Tuble2String("你是谁哈屁","11"),
+                new Tuble2String("你是谁哈哈","11"),
+                new Tuble2String("你是谁哈狗屁","11"),
+            };
+            var result = list.SearchModels(strChar, new String[] { "Item1" });
+            result.ForEach((s) => Console.WriteLine(s.Item1));
         }
     }
 }
---
+++ src/Extter/MediaCaller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace System.Data.Extter
+{
+    /// <summary>
+    /// 媒介调用
+    /// </summary>
+    public static class MediaCaller
+    {
+        /// <summary>
+        /// 转换成字节数组
+        /// </summary>
+        /// <returns></returns>
+        public static byte[] GetBytes(this MemoryStream ms)
+        {
+            ms.Seek(0, SeekOrigin.Begin); //一定不要忘记将流的初始位置重置
+            byte[] bytes = new byte[ms.Length];
+            ms.Read(bytes, 0, bytes.Length); //如果上面流没有seek 则这里读取的数据全会为0
+            ms.Dispose();
+            return bytes;
+        }
+    }
+}
