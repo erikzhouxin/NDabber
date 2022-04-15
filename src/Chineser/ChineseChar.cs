98b1c44b8ea189d9fef9251748d85d363b522515
ErikZhouXin@EZhouXin.com <ErikZhouXin@EZhouXin.com>
2022-04-15 13:45:03 +0800

    [ErikZhouXin]添加中文转拼音支持

---
+++ src/Chineser/ChineseChar.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace System.Data.Chineser
+{
+    /// <summary>
+    /// 封装了简体中文的读音和笔画等基本信息。
+    /// </summary>
+    public class ChineseChar
+    {
+        private const short MaxPolyphoneNum = 8;
+
+        private static PinYinConverter.CharDictionary charDictionary;
+
+        private static PinYinConverter.PinyinDictionary pinyinDictionary;
+
+        private static PinYinConverter.StrokeDictionary strokeDictionary;
+
+        private static PinYinConverter.HomophoneDictionary homophoneDictionary;
+
+        private char chineseCharacter;
+
+        private short strokeNumber;
+
+        private bool isPolyphone;
+
+        private short pinyinCount;
+
+        private string[] pinyinList = new string[8];
+        /// <summary>
+        /// 获取这个字符的拼音个数。
+        /// </summary>
+        public short PinyinCount
+        {
+            get
+            {
+                return this.pinyinCount;
+            }
+        }
+        /// <summary>
+        /// 获取这个字符的笔画数。
+        /// </summary>
+        public short StrokeNumber
+        {
+            get
+            {
+                return this.strokeNumber;
+            }
+        }
+        /// <summary>
+        /// 获取这个字符是否是多音字。
+        /// </summary>
+        public bool IsPolyphone
+        {
+            get
+            {
+                return this.isPolyphone;
+            }
+        }
+        /// <summary>
+        /// 获取这个字符的拼音。该集合长度不能表示实际拼音个数，实际拼音个数请使用PinyinCount字段
+        /// </summary>
+        public ReadOnlyCollection<string> Pinyins
+        {
+            get
+            {
+                return new ReadOnlyCollection<string>(this.pinyinList);
+            }
+        }
+        /// <summary>
+        /// 获取这个汉字字符。
+        /// </summary>
+        public char ChineseCharacter
+        {
+            get
+            {
+                return this.chineseCharacter;
+            }
+        }
+
+        static ChineseChar()
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            using (BinaryReader binaryReader = new BinaryReader(new MemoryStream(System.Data.Properties.Resources.PinyinDictionary)))
+            {
+                ChineseChar.pinyinDictionary = PinYinConverter.PinyinDictionary.Deserialize(binaryReader);
+            }
+            using (BinaryReader binaryReader2 = new BinaryReader(new MemoryStream(System.Data.Properties.Resources.CharDictionary)))
+            {
+                ChineseChar.charDictionary = PinYinConverter.CharDictionary.Deserialize(binaryReader2);
+            }
+            using (BinaryReader binaryReader3 = new BinaryReader(new MemoryStream(System.Data.Properties.Resources.HomophoneDictionary)))
+            {
+                ChineseChar.homophoneDictionary = PinYinConverter.HomophoneDictionary.Deserialize(binaryReader3);
+            }
+            using (BinaryReader binaryReader4 = new BinaryReader(new MemoryStream(System.Data.Properties.Resources.StrokeDictionary)))
+            {
+                ChineseChar.strokeDictionary = PinYinConverter.StrokeDictionary.Deserialize(binaryReader4);
+            }
+        }
+        /// <summary>
+        /// ChineseChar类的构造函数。
+        /// </summary>
+        /// <param name="ch">指定的汉字字符。</param>
+        public ChineseChar(char ch)
+        {
+            if (!ChineseChar.IsValidChar(ch))
+            {
+                throw new NotSupportedException(PinYinConverter.CHARACTER_NOT_SUPPORTED);
+            }
+            this.chineseCharacter = ch;
+            var charUnit = ChineseChar.charDictionary.GetCharUnit(ch);
+            this.strokeNumber = (short)charUnit.StrokeNumber;
+            this.pinyinCount = (short)charUnit.PinyinCount;
+            this.isPolyphone = (charUnit.PinyinCount > 1);
+            for (int i = 0; i < (int)this.pinyinCount; i++)
+            {
+                var pinYinUnitByIndex = ChineseChar.pinyinDictionary.GetPinYinUnitByIndex((int)charUnit.PinyinIndexList[i]);
+                this.pinyinList[i] = pinYinUnitByIndex.Pinyin;
+            }
+        }
+        /// <summary>
+        /// 识别字符是否有指定的读音。
+        /// </summary>
+        /// <param name="pinyin">指定的需要被识别的拼音</param>
+        /// <returns>如果指定的拼音字符串在实例字符的拼音集合中则返回ture，否则返回false。 </returns>
+        public bool HasSound(string pinyin)
+        {
+            if (pinyin == null)
+            {
+                throw new ArgumentNullException("HasSound_pinyin");
+            }
+            for (int i = 0; i < (int)this.PinyinCount; i++)
+            {
+                if (string.Compare(this.Pinyins[i], pinyin, true, CultureInfo.CurrentCulture) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 识别给出的字符是否是实例字符的同音字。 
+        /// </summary>
+        /// <param name="ch">指出需要识别的字符。</param>
+        /// <returns>如果给出的实例字符是同音字则返回ture，否则返回false。</returns>
+        public bool IsHomophone(char ch)
+        {
+            return ChineseChar.IsHomophone(this.chineseCharacter, ch);
+        }
+        /// <summary>
+        /// 识别给出的两个字符是否是同音字。
+        /// </summary>
+        /// <param name="ch1">指出第一个字符</param>
+        /// <param name="ch2">指出第二个字符</param>
+        /// <returns>如果给出的字符是同音字返回ture，否则返回false。</returns>
+        public static bool IsHomophone(char ch1, char ch2)
+        {
+            var charUnit = ChineseChar.charDictionary.GetCharUnit(ch1);
+            var charUnit2 = ChineseChar.charDictionary.GetCharUnit(ch2);
+            return ChineseChar.ExistSameElement<short>(charUnit.PinyinIndexList, charUnit2.PinyinIndexList);
+        }
+        /// <summary>
+        /// 将给出的字符和实例字符的笔画数进行比较。
+        /// </summary>
+        /// <param name="ch">显示给出的字符</param>
+        /// <returns>说明比较操作的结果。 如果给出字符和实例字符的笔画数相同，返回值为 0。 如果实例字符比给出字符的笔画多，返回值为> 0。 如果实例字符比给出字符的笔画少，返回值为 &lt; 0。 </returns>
+        public int CompareStrokeNumber(char ch)
+        {
+            var charUnit = ChineseChar.charDictionary.GetCharUnit(ch);
+            return (int)(this.StrokeNumber - (short)charUnit.StrokeNumber);
+        }
+        /// <summary>
+        /// 获取给定拼音的所有同音字。
+        /// </summary>
+        /// <param name="pinyin">指出读音。</param>
+        /// <returns>返回具有相同的指定拼音的字符串列表。 如果拼音不是有效值则返回空。 </returns>
+        public static char[] GetChars(string pinyin)
+        {
+            if (pinyin == null)
+            {
+                throw new ArgumentNullException("pinyin");
+            }
+            if (!ChineseChar.IsValidPinyin(pinyin))
+            {
+                return null;
+            }
+            var homophoneUnit = ChineseChar.homophoneDictionary.GetHomophoneUnit(ChineseChar.pinyinDictionary, pinyin);
+            return homophoneUnit.HomophoneList;
+        }
+        /// <summary>
+        /// 识别给出的拼音是否是一个有效的拼音字符串。
+        /// </summary>
+        /// <param name="pinyin">指出需要识别的字符串。</param>
+        /// <returns>如果指定的字符串是一个有效的拼音字符串则返回ture，否则返回false。</returns>
+        public static bool IsValidPinyin(string pinyin)
+        {
+            if (pinyin == null)
+            {
+                throw new ArgumentNullException("pinyin");
+            }
+            return ChineseChar.pinyinDictionary.GetPinYinUnitIndex(pinyin) >= 0;
+        }
+        /// <summary>
+        /// 识别给出的字符串是否是一个有效的汉字字符。
+        /// </summary>
+        /// <param name="ch">指出需要识别的字符。</param>
+        /// <returns>如果指定的字符是一个有效的汉字字符则返回ture，否则返回false。</returns>
+        public static bool IsValidChar(char ch)
+        {
+            var charUnit = ChineseChar.charDictionary.GetCharUnit(ch);
+            return charUnit != null;
+        }
+        /// <summary>
+        /// 识别给出的笔画数是否是一个有效的笔画数。
+        /// </summary>
+        /// <param name="strokeNumber">指出需要识别的笔画数。</param>
+        /// <returns>如果指定的笔画数是一个有效的笔画数则返回ture，否则返回false。</returns>
+        public static bool IsValidStrokeNumber(short strokeNumber)
+        {
+            if (strokeNumber < 0 || strokeNumber > 48)
+            {
+                return false;
+            }
+            var strokeUnit = ChineseChar.strokeDictionary.GetStrokeUnit((int)strokeNumber);
+            return strokeUnit != null;
+        }
+        /// <summary>
+        /// 检索具有指定拼音的字符数。
+        /// </summary>
+        /// <param name="pinyin">显示需要被识别的拼音字符串。</param>
+        /// <returns>返回具有指定拼音的字符数。 如果拼音不是有效值则返回-1。</returns>
+        public static short GetHomophoneCount(string pinyin)
+        {
+            if (pinyin == null)
+            {
+                throw new ArgumentNullException("pinyin");
+            }
+            if (!ChineseChar.IsValidPinyin(pinyin))
+            {
+                return -1;
+            }
+            return ChineseChar.homophoneDictionary.GetHomophoneUnit(ChineseChar.pinyinDictionary, pinyin).Count;
+        }
+        /// <summary>
+        /// 检索指定字符的笔画数。 
+        /// </summary>
+        /// <param name="ch">指出需要识别的字符。</param>
+        /// <returns>返回指定字符的笔画数。 如果字符不是有效值则返回-1。 </returns>
+        public static short GetStrokeNumber(char ch)
+        {
+            if (!ChineseChar.IsValidChar(ch))
+            {
+                return -1;
+            }
+            var charUnit = ChineseChar.charDictionary.GetCharUnit(ch);
+            return (short)charUnit.StrokeNumber;
+        }
+        /// <summary>
+        /// 检索具有指定笔画数的所有字符串。
+        /// </summary>
+        /// <param name="strokeNumber">指出需要被识别的笔画数。</param>
+        /// <returns>返回具有指定笔画数的字符列表。 如果笔画数是无效值返回空。</returns>
+        public static char[] GetChars(short strokeNumber)
+        {
+            if (!ChineseChar.IsValidStrokeNumber(strokeNumber))
+            {
+                return null;
+            }
+            var strokeUnit = ChineseChar.strokeDictionary.GetStrokeUnit((int)strokeNumber);
+            return strokeUnit.CharList;
+        }
+        /// <summary>
+        /// 检索具有指定笔画数的字符个数。
+        /// </summary>
+        /// <param name="strokeNumber">显示需要被识别的笔画数。</param>
+        /// <returns>返回具有指定笔画数的字符数。 如果笔画数是无效值返回-1。</returns>
+        public static short GetCharCount(short strokeNumber)
+        {
+            if (!ChineseChar.IsValidStrokeNumber(strokeNumber))
+            {
+                return -1;
+            }
+            return ChineseChar.strokeDictionary.GetStrokeUnit((int)strokeNumber).CharCount;
+        }
+
+        private static bool ExistSameElement<T>(T[] array1, T[] array2) where T : IComparable
+        {
+            int num = 0;
+            int num2 = 0;
+            while (num < array1.Length && num2 < array2.Length)
+            {
+                if (array1[num].CompareTo(array2[num2]) < 0)
+                {
+                    num++;
+                }
+                else
+                {
+                    if (array1[num].CompareTo(array2[num2]) <= 0)
+                    {
+                        return true;
+                    }
+                    num2++;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Chineser/PinYinConverter.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace System.Data.Chineser
+{
+    internal partial class PinYinConverter
+    {
+        public static String CHARACTER_NOT_SUPPORTED { get => "The character is not in extended character set of Simplified Chinese."; }
+        public static String EXCEED_BORDER_EXCEPTION { get => "The number of strokes cannot be a negative value or greater than the maximum number of strokes  in the Chinese character."; }
+        public static String INDEX_OUT_OF_RANGE { get => "The index is out of range."; }
+
+		internal class CharDictionary
+		{
+			internal int Length;
+
+			internal int Count;
+
+			internal short Offset;
+
+			internal readonly byte[] Reserved = new byte[24];
+
+			internal List<CharUnit> CharUnitTable;
+
+			internal readonly short EndMark = 32767;
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Length);
+				binaryWriter.Write(this.Count);
+				binaryWriter.Write(this.Offset);
+				binaryWriter.Write(this.Reserved);
+				for (int i = 0; i < this.Count; i++)
+				{
+					this.CharUnitTable[i].Serialize(binaryWriter);
+				}
+				binaryWriter.Write(this.EndMark);
+			}
+
+			internal static CharDictionary Deserialize(BinaryReader binaryReader)
+			{
+				CharDictionary charDictionary = new CharDictionary();
+				binaryReader.ReadInt32();
+				charDictionary.Length = binaryReader.ReadInt32();
+				charDictionary.Count = binaryReader.ReadInt32();
+				charDictionary.Offset = binaryReader.ReadInt16();
+				binaryReader.ReadBytes(24);
+				charDictionary.CharUnitTable = new List<CharUnit>();
+				for (int i = 0; i < charDictionary.Count; i++)
+				{
+					charDictionary.CharUnitTable.Add(CharUnit.Deserialize(binaryReader));
+				}
+				binaryReader.ReadInt16();
+				return charDictionary;
+			}
+
+			internal CharUnit GetCharUnit(int index)
+			{
+				if (index < 0 || index >= this.Count)
+				{
+					throw new ArgumentOutOfRangeException("index", INDEX_OUT_OF_RANGE);
+				}
+				return this.CharUnitTable[index];
+			}
+
+			internal CharUnit GetCharUnit(char ch)
+			{
+				CharUnitPredicate @object = new CharUnitPredicate(ch);
+				return this.CharUnitTable.Find(new Predicate<CharUnit>(@object.Match));
+			}
+		}
+		internal class CharUnit
+		{
+			internal char Char;
+
+			internal byte StrokeNumber;
+
+			internal byte PinyinCount;
+
+			internal short[] PinyinIndexList;
+
+			internal static CharUnit Deserialize(BinaryReader binaryReader)
+			{
+				CharUnit charUnit = new CharUnit();
+				charUnit.Char = binaryReader.ReadChar();
+				charUnit.StrokeNumber = binaryReader.ReadByte();
+				charUnit.PinyinCount = binaryReader.ReadByte();
+				charUnit.PinyinIndexList = new short[(int)charUnit.PinyinCount];
+				for (int i = 0; i < (int)charUnit.PinyinCount; i++)
+				{
+					charUnit.PinyinIndexList[i] = binaryReader.ReadInt16();
+				}
+				return charUnit;
+			}
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Char);
+				binaryWriter.Write(this.StrokeNumber);
+				binaryWriter.Write(this.PinyinCount);
+				for (int i = 0; i < (int)this.PinyinCount; i++)
+				{
+					binaryWriter.Write(this.PinyinIndexList[i]);
+				}
+			}
+		}
+		internal class CharUnitPredicate
+		{
+			private char ExpectedChar;
+
+			internal CharUnitPredicate(char ch)
+			{
+				this.ExpectedChar = ch;
+			}
+
+			internal bool Match(CharUnit charUnit)
+			{
+				return charUnit.Char == this.ExpectedChar;
+			}
+		}
+		internal class HomophoneDictionary
+		{
+			internal int Length;
+
+			internal short Offset;
+
+			internal short Count;
+
+			internal readonly byte[] Reserved = new byte[8];
+
+			internal List<HomophoneUnit> HomophoneUnitTable;
+
+			internal readonly short EndMark = 32767;
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Length);
+				binaryWriter.Write(this.Count);
+				binaryWriter.Write(this.Offset);
+				binaryWriter.Write(this.Reserved);
+				for (int i = 0; i < (int)this.Count; i++)
+				{
+					this.HomophoneUnitTable[i].Serialize(binaryWriter);
+				}
+				binaryWriter.Write(this.EndMark);
+			}
+
+			internal static HomophoneDictionary Deserialize(BinaryReader binaryReader)
+			{
+				HomophoneDictionary homophoneDictionary = new HomophoneDictionary();
+				binaryReader.ReadInt32();
+				homophoneDictionary.Length = binaryReader.ReadInt32();
+				homophoneDictionary.Count = binaryReader.ReadInt16();
+				homophoneDictionary.Offset = binaryReader.ReadInt16();
+				binaryReader.ReadBytes(8);
+				homophoneDictionary.HomophoneUnitTable = new List<HomophoneUnit>();
+				for (int i = 0; i < (int)homophoneDictionary.Count; i++)
+				{
+					homophoneDictionary.HomophoneUnitTable.Add(HomophoneUnit.Deserialize(binaryReader));
+				}
+				binaryReader.ReadInt16();
+				return homophoneDictionary;
+			}
+
+			internal HomophoneUnit GetHomophoneUnit(int index)
+			{
+				if (index < 0 || index >= (int)this.Count)
+				{
+					throw new ArgumentOutOfRangeException("index", INDEX_OUT_OF_RANGE);
+				}
+				return this.HomophoneUnitTable[index];
+			}
+
+			internal HomophoneUnit GetHomophoneUnit(PinyinDictionary pinyinDictionary, string pinyin)
+			{
+				return this.GetHomophoneUnit(pinyinDictionary.GetPinYinUnitIndex(pinyin));
+			}
+		}
+		internal class HomophoneUnit
+		{
+			internal short Count;
+
+			internal char[] HomophoneList;
+
+			internal static HomophoneUnit Deserialize(BinaryReader binaryReader)
+			{
+				HomophoneUnit homophoneUnit = new HomophoneUnit();
+				homophoneUnit.Count = binaryReader.ReadInt16();
+				homophoneUnit.HomophoneList = new char[(int)homophoneUnit.Count];
+				for (int i = 0; i < (int)homophoneUnit.Count; i++)
+				{
+					homophoneUnit.HomophoneList[i] = binaryReader.ReadChar();
+				}
+				return homophoneUnit;
+			}
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Count);
+				for (int i = 0; i < (int)this.Count; i++)
+				{
+					binaryWriter.Write(this.HomophoneList[i]);
+				}
+			}
+		}
+		internal class PinyinDictionary
+		{
+			internal short Length;
+
+			internal short Count;
+
+			internal short Offset;
+
+			internal readonly byte[] Reserved = new byte[8];
+
+			internal List<PinyinUnit> PinyinUnitTable;
+
+			internal readonly short EndMark = 32767;
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Length);
+				binaryWriter.Write(this.Count);
+				binaryWriter.Write(this.Offset);
+				binaryWriter.Write(this.Reserved);
+				for (int i = 0; i < (int)this.Count; i++)
+				{
+					this.PinyinUnitTable[i].Serialize(binaryWriter);
+				}
+				binaryWriter.Write(this.EndMark);
+			}
+
+			internal static PinyinDictionary Deserialize(BinaryReader binaryReader)
+			{
+				PinyinDictionary pinyinDictionary = new PinyinDictionary();
+				binaryReader.ReadInt32();
+				pinyinDictionary.Length = binaryReader.ReadInt16();
+				pinyinDictionary.Count = binaryReader.ReadInt16();
+				pinyinDictionary.Offset = binaryReader.ReadInt16();
+				binaryReader.ReadBytes(8);
+				pinyinDictionary.PinyinUnitTable = new List<PinyinUnit>();
+				for (int i = 0; i < (int)pinyinDictionary.Count; i++)
+				{
+					pinyinDictionary.PinyinUnitTable.Add(PinyinUnit.Deserialize(binaryReader));
+				}
+				binaryReader.ReadInt16();
+				return pinyinDictionary;
+			}
+
+			internal int GetPinYinUnitIndex(string pinyin)
+			{
+				PinyinUnitPredicate @object = new PinyinUnitPredicate(pinyin);
+				return this.PinyinUnitTable.FindIndex(new Predicate<PinyinUnit>(@object.Match));
+			}
+
+			internal PinyinUnit GetPinYinUnit(string pinyin)
+			{
+				PinyinUnitPredicate @object = new PinyinUnitPredicate(pinyin);
+				return this.PinyinUnitTable.Find(new Predicate<PinyinUnit>(@object.Match));
+			}
+
+			internal PinyinUnit GetPinYinUnitByIndex(int index)
+			{
+				if (index < 0 || index >= (int)this.Count)
+				{
+					throw new ArgumentOutOfRangeException("index", INDEX_OUT_OF_RANGE);
+				}
+				return this.PinyinUnitTable[index];
+			}
+		}
+		internal class PinyinUnit
+		{
+			internal string Pinyin;
+
+			internal static PinyinUnit Deserialize(BinaryReader binaryReader)
+			{
+				PinyinUnit pinyinUnit = new PinyinUnit();
+				byte[] bytes = binaryReader.ReadBytes(7);
+				pinyinUnit.Pinyin = Encoding.ASCII.GetString(bytes, 0, 7);
+				char[] array = new char[1];
+				char[] trimChars = array;
+				pinyinUnit.Pinyin = pinyinUnit.Pinyin.TrimEnd(trimChars);
+				return pinyinUnit;
+			}
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				byte[] array = new byte[7];
+				Encoding.ASCII.GetBytes(this.Pinyin, 0, this.Pinyin.Length, array, 0);
+				binaryWriter.Write(array);
+			}
+		}
+		internal class PinyinUnitPredicate
+		{
+			private string ExpectedPinyin;
+
+			internal PinyinUnitPredicate(string pinyin)
+			{
+				this.ExpectedPinyin = pinyin;
+			}
+
+			internal bool Match(PinyinUnit pinyinUnit)
+			{
+				return string.Compare(pinyinUnit.Pinyin, this.ExpectedPinyin, true, CultureInfo.CurrentCulture) == 0;
+			}
+		}
+		internal class StrokeDictionary
+		{
+			internal const short MaxStrokeNumber = 48;
+
+			internal int Length;
+
+			internal int Count;
+
+			internal short Offset;
+
+			internal readonly byte[] Reserved = new byte[24];
+
+			internal List<StrokeUnit> StrokeUnitTable;
+
+			internal readonly short EndMark = 32767;
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				binaryWriter.Write(this.Length);
+				binaryWriter.Write(this.Count);
+				binaryWriter.Write(this.Offset);
+				binaryWriter.Write(this.Reserved);
+				for (int i = 0; i < this.Count; i++)
+				{
+					this.StrokeUnitTable[i].Serialize(binaryWriter);
+				}
+				binaryWriter.Write(this.EndMark);
+			}
+
+			internal static StrokeDictionary Deserialize(BinaryReader binaryReader)
+			{
+				StrokeDictionary strokeDictionary = new StrokeDictionary();
+				binaryReader.ReadInt32();
+				strokeDictionary.Length = binaryReader.ReadInt32();
+				strokeDictionary.Count = binaryReader.ReadInt32();
+				strokeDictionary.Offset = binaryReader.ReadInt16();
+				binaryReader.ReadBytes(24);
+				strokeDictionary.StrokeUnitTable = new List<StrokeUnit>();
+				for (int i = 0; i < strokeDictionary.Count; i++)
+				{
+					strokeDictionary.StrokeUnitTable.Add(StrokeUnit.Deserialize(binaryReader));
+				}
+				binaryReader.ReadInt16();
+				return strokeDictionary;
+			}
+
+			internal StrokeUnit GetStrokeUnitByIndex(int index)
+			{
+				if (index < 0 || index >= this.Count)
+				{
+					throw new ArgumentOutOfRangeException("index", INDEX_OUT_OF_RANGE);
+				}
+				return this.StrokeUnitTable[index];
+			}
+
+			internal StrokeUnit GetStrokeUnit(int strokeNum)
+			{
+				if (strokeNum <= 0 || strokeNum > 48)
+				{
+					throw new ArgumentOutOfRangeException("strokeNum");
+				}
+				StrokeUnitPredicate @object = new StrokeUnitPredicate(strokeNum);
+				return this.StrokeUnitTable.Find(new Predicate<StrokeUnit>(@object.Match));
+			}
+		}
+		internal class StrokeUnit
+		{
+			internal byte StrokeNumber;
+
+			internal short CharCount;
+
+			internal char[] CharList;
+
+			internal static StrokeUnit Deserialize(BinaryReader binaryReader)
+			{
+				StrokeUnit strokeUnit = new StrokeUnit();
+				strokeUnit.StrokeNumber = binaryReader.ReadByte();
+				strokeUnit.CharCount = binaryReader.ReadInt16();
+				strokeUnit.CharList = new char[(int)strokeUnit.CharCount];
+				for (int i = 0; i < (int)strokeUnit.CharCount; i++)
+				{
+					strokeUnit.CharList[i] = binaryReader.ReadChar();
+				}
+				return strokeUnit;
+			}
+
+			internal void Serialize(BinaryWriter binaryWriter)
+			{
+				if (this.CharCount == 0)
+				{
+					return;
+				}
+				binaryWriter.Write(this.StrokeNumber);
+				binaryWriter.Write(this.CharCount);
+				binaryWriter.Write(this.CharList);
+			}
+		}
+		internal class StrokeUnitPredicate
+		{
+			private int ExpectedStrokeNum;
+
+			internal StrokeUnitPredicate(int strokeNum)
+			{
+				this.ExpectedStrokeNum = strokeNum;
+			}
+
+			internal bool Match(StrokeUnit strokeUnit)
+			{
+				return (int)strokeUnit.StrokeNumber == this.ExpectedStrokeNum;
+			}
+		}
+	}
+}
