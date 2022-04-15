21ecf4c66fb48356af153209e007b6b064530056
ErikZhouXin@EZhouXin.com <ErikZhouXin@EZhouXin.com>
2022-04-15 13:45:03 +0800

    [ErikZhouXin]添加中文转拼音支持

---
+++ demo/ut/PinYinUT/PinYinConverterUT.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Chineser;
+using System.Text;
+
+namespace System.Data.DabberUT.PinYinUT
+{
+    /// <summary>
+    /// 拼音转换测试
+    /// </summary>
+    [TestClass]
+    public class PinYinConverterUT
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.IsTrue(ChineseChar.IsValidPinyin("ni"));
+            Assert.IsTrue(ChineseChar.IsValidChar('啊'));
+        }
+    }
+}
