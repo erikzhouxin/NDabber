b2bd4351b235ff70eb746d384e58c2c664745d8b
ErikZhouXin <ErikZhouXin@EZhouXin.com>
2022-12-30 17:25:46 +0800

    [ErikZhouXin]扩展兼容.NET40的获取反射Attributes方法

--- src/Cobber/CompatAssist.cs
+++ src/Cobber/CompatAssist.cs
@@ -19,6 +19,20 @@ namespace System.Data.Cobber
         /// <param name="member"></param>
         /// <param name="isHerit"></param>
         /// <returns></returns>
+        public static IEnumerable<T> GetCustomAttributes<T>(this MemberInfo member, bool isHerit = false) where T : Attribute
+        {
+            foreach (var attr in member.GetCustomAttributes(typeof(T), isHerit))
+            {
+                if (attr is T real) { yield return real; }
+            }
+        }
+        /// <summary>
+        /// 获取自定义属性
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="member"></param>
+        /// <param name="isHerit"></param>
+        /// <returns></returns>
         public static T GetCustomAttribute<T>(this MemberInfo member, bool isHerit = false) where T : Attribute
         {
             foreach (var attr in member.GetCustomAttributes(typeof(T), isHerit))
@@ -46,6 +60,20 @@ namespace System.Data.Cobber
         /// 获取用户属性
         /// </summary>
         /// <typeparam name="T"></typeparam>
+        /// <param name="prop"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetCustomAttributes<T>(this PropertyInfo prop, bool inherit = false) where T : Attribute
+        {
+            foreach (var item in prop.GetCustomAttributes(inherit))
+            {
+                if (item is T) { yield return item as T; }
+            }
+        }
+        /// <summary>
+        /// 获取用户属性
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="type"></param>
         /// <param name="inherit"></param>
         /// <returns></returns>
@@ -56,6 +84,20 @@ namespace System.Data.Cobber
                 if (item is T) { return item as T; }
             }
             return default(T);
+        }
+        /// <summary>
+        /// 获取用户属性
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetCustomAttributes<T>(this Type type, bool inherit = false) where T : Attribute
+        {
+            foreach (var item in type.GetCustomAttributes(inherit))
+            {
+                if (item is T) { yield return item as T; }
+            }
         }
 #endif
     }
